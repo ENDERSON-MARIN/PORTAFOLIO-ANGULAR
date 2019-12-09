d191e99c4cf93740f0a9db946e6660d4395c26cf
ENDERSON-MARIN <marinenderson1@gmail.com>
2019-12-09 07:48:14 -0500

    work in the about and services

--- src/app/services/infopagina.service.ts
+++ src/app/services/infopagina.service.ts
@@ -9,16 +9,36 @@ export class InfopaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[]=[];
+
   constructor(private http: HttpClient) {
     console.log("servicio de infopagina Listo");
+    this.cargarInfo();
+    this.cargarEquipo();
 
-    //Leer el archivo json
+  }
+
+  private cargarInfo(){
+    
+    //Leer el archivo json local
 
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
+       // console.log(resp);
+      
+    });
+  }
+
+  private cargarEquipo(){
+    
+    //Leer el archivo json de firebase
+
+    this.http.get('https://portafolio-angular-f1ea3.firebaseio.com/equipo.json')
+    .subscribe( (resp:any[]) => {
+    this.equipo=resp;
+      // console.log(resp);
     });
   }
 }
